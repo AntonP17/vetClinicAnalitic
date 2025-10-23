79c444cc7ad83230d7fdcda61a94a55c8c7495c8
AntonP17 <pokrovskii.anton@mail.ru>
2025-10-23 23:04:53 +0300

    add eureka discovey client

--- src/main/java/by/antohakon/visitsanalitics/VisitsAnaliticsApplication.java
+++ src/main/java/by/antohakon/visitsanalitics/VisitsAnaliticsApplication.java
@@ -2,8 +2,10 @@ package by.antohakon.visitsanalitics;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class VisitsAnaliticsApplication {
 
     public static void main(String[] args) {
